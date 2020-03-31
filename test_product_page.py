0081d6910441b1137bb39bd95c0c0cdb71881df5
Maria Bulatova <maria.r.bulatova@yandex.ru>
2020-03-31 17:20:32 +0300

    Create not parametrized test for review

--- test_product_page.py
+++ test_product_page.py
@@ -10,6 +10,15 @@ import faker
 PASSWORD_LEN = 12
 
 @pytest.mark.need_review
+def test_guest_can_add_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
+    page = ProductPage(browser, link)
+    page.open()
+    page.press_button_add_product_to_basket()
+    page.should_be_correct_product_name()
+    page.should_be_message_added_to_basket()
+    page.should_be_same_price_product_and_bucket()
+
 @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
@@ -20,7 +29,7 @@ PASSWORD_LEN = 12
                                   pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks=pytest.mark.xfail),
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
-def test_guest_can_add_product_to_basket(browser, link):
+def test_guest_can_add_product_to_basket_offers(browser, link):
     page = ProductPage(browser, link)
     page.open()
     page.press_button_add_product_to_basket()
